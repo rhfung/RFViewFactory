98e62f49e6e2f375049333b006e0b6f1db4ea75d
Richard H Fung <contact@rhfung.com>
2013-11-25 18:46:43 -0800

    added helper methods for view controllers

--- RFViewFactory/UINavigationController+PopByName.h
+++ RFViewFactory/UINavigationController+PopByName.h
@@ -10,7 +10,17 @@
 
 @interface UINavigationController (RFViewFactory_PopByName)
 
+/// Pop several view controllers on the stack.
 -(NSArray*)popToViewControllerNamed:(NSString*)viewControllerName animated:(BOOL)animated;
+
+/// Find the earliest view controller pushed on the stack with a given name.
+-(UIViewController*)findFirstViewControllerNamed:(NSString*)viewControllerName;
+
+/// Find the most recently seen view controller with a name.
+-(UIViewController*)findLastViewControllerNamed:(NSString*)viewControllerName;
+
+/// Find all view controllers that match a given class.
+-(NSArray*)findViewControllersNamed:(NSString*)viewControllerName;
 
 @end
 
--- RFViewFactory/UINavigationController+PopByName.m
+++ RFViewFactory/UINavigationController+PopByName.m
@@ -12,16 +12,7 @@
 
 -(NSArray*)popToViewControllerNamed:(NSString*)viewControllerName animated:(BOOL)animated
 {
-  NSMutableArray* arrVCs = [[NSMutableArray alloc] init];
-  
-  // check all view controllers for vc's of the right type
-  for (UIViewController* s in self.viewControllers)
-  {
-    if ([NSStringFromClass(s.class) isEqualToString:viewControllerName])
-    {
-      [arrVCs addObject:s];
-    }
-  }
+  NSArray* arrVCs = [self findViewControllersNamed:viewControllerName];
   
   if (arrVCs.count == 0)
     return nil;
@@ -31,6 +22,39 @@
     // Thus, the closest matching object is the last one in the array that we built.
     return [self popToViewController:[arrVCs lastObject] animated:animated];
   }
+}
+
+-(UIViewController*)findFirstViewControllerNamed:(NSString*)viewControllerName {
+  NSArray* arrResults = [self findViewControllersNamed:viewControllerName];
+  if (arrResults.count > 0)
+  {
+    return arrResults[0];
+  }
+  return nil;
+}
+
+-(UIViewController*)findLastViewControllerNamed:(NSString*)viewControllerName {
+  NSArray* arrResults = [self findViewControllersNamed:viewControllerName];
+  if (arrResults.count > 0)
+  {
+    return arrResults.lastObject;
+  }
+  return nil;
+}
+
+-(NSArray*)findViewControllersNamed:(NSString*)viewControllerName {
+  NSMutableArray* arrVCs = [[NSMutableArray alloc] init];
+
+  // check all view controllers for vc's of the right type
+  for (UIViewController* s in self.viewControllers)
+  {
+    if ([NSStringFromClass(s.class) isEqualToString:viewControllerName])
+    {
+      [arrVCs addObject:s];
+    }
+  }
+
+  return arrVCs;
 }
 
 @end
